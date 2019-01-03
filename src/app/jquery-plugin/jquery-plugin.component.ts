40a3add5e6ea1ab57ff5dedb489dd73bf406eb35
youngstone <youngstone89@gmail.com>
2019-01-04 01:44:20 +0900

    jquery test completed

--- src/app/jquery-plugin/jquery-plugin.component.ts
+++ src/app/jquery-plugin/jquery-plugin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-jquery-plugin',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JqueryPluginComponent implements OnInit {
 
+  title = 'Look jQuery Animation working in action!';
+
   constructor() { }
 
   ngOnInit() {
+
+      $(document).ready(function () {
+        $('button').click(function() {
+          console.log('button clicked')
+          const div = $('div#jquery-div');
+          console.log('div', div);
+          div.animate({'left': '100px'}, 'slow');
+          div.animate({'font-size': '5em'}, 'slow');
+        });
+      });
+
+
   }
 
 }
